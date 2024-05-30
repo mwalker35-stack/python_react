52e895d5c5f477557afe029f765432f57802cee1
mwalker35-stack <mwalke35@yahoo.com>
2024-05-29 22:44:48 -0400

    added routes for get delete update, front end added to show contact info but no contacts yet

--- backend/main.py
+++ backend/main.py
@@ -26,6 +26,35 @@ def create_contact():
     
     return jsonify({"message": "Good Job User created!"}), 201
 
+@app.route("/update_contact/<int:user_id>")
+def update_contact(user_id):
+    contact=Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    data = request.json
+    contact.first_name = data.get("firstName", contact.first_name)
+    contact.last_name = data.get("lastName", contact.last_name)
+    contact.email = data.get("email", contact.email)
+
+    db.session.commit()
+
+    return jsonify({"message":"User updated."}), 200
+
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
+def delete_contact(user_id):
+    contact=Contact.query.get(user_id)
+
+    if not contact:
+        return jsonify({"message": "User not found"}), 404
+    
+    db.session.delete(contact)
+    db.session.commit()
+
+    return jsonify({"message": "User deleted!"}), 200
+    
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
